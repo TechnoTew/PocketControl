9fa2e5824bb12c8e78a9baa6b573987a2016727b
TechnoTew <jingan0904@gmail.com>
2021-11-07 17:08:03 +0800

    Splash Screen built

---
+++ app/src/main/java/com/example/pocketcontrol/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.example.pocketcontrol;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Launch the layout -> splash.xml
+        setContentView(R.layout.splash);
+        Thread splashThread = new Thread() {
+
+            public void run() {
+                try {
+                    getSupportActionBar().hide(); //hide the title bar.
+                    // sleep time in milliseconds
+                    sleep(1500);
+
+                }  catch(InterruptedException e) {
+                    // Trace the error
+                    e.printStackTrace();
+                } finally
+                {
+                    // Launch the MainActivity class
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        };
+        // To Start the thread
+        splashThread.start();
+    }
+}
