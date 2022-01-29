238bb5a2253d39888e44409e8a28bbff5895b4dd
TechnoTew <jingan0904@gmail.com>
2022-01-29 22:16:34 +0800

    Initialize Ad engine to stop error

--- app/src/main/java/Pages/FirstTimeSetup.java
+++ app/src/main/java/Pages/FirstTimeSetup.java
@@ -21,6 +21,8 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
@@ -38,6 +40,13 @@ public class FirstTimeSetup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time_setup);
+
+        // initialize ad module
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
 
         // uncomment should u need to rebuild whole database
         // getApplicationContext().deleteDatabase("PocketControl");
